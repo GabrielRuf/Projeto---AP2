05cd0dca626d32eab64f334642692165115deb19
Gabriel <gabriel.rufino05@gmail.com>
2022-10-20 00:06:45 -0300

    98% do submenu clientes pronto

--- Projeto - AP2.cpp
+++ Projeto - AP2.cpp
@@ -5,7 +5,7 @@
 
 struct usuario
 {
-	char nome[40],rua[40],emails[3][40],telefone[3][15],profissao[40],datanasc[10],cpf[15],nro[15],cep[15];
+	char nome[40],emails[3][40],telefone[3][15],profissao[40],datanasc[10],cpf[15],nro[15],cep[15];
 	
 }usuarios[20];
 
@@ -21,10 +21,10 @@ int buscar(usuario *Usuarios,int pos,char *cpf)
 	return -1;
 }
 
-void incluir_usuario(usuario *Usuarios,int *qtdCadastro) //Falta fazer a verificação do CPF!!!!
+void incluir_usuario(usuario *Usuarios,int *qtdCadastro)
 {
 	system("cls");
-	char cpf[40];
+	char cpf[15];
 	struct usuario user;
 	int aut;
 	fflush(stdin);
@@ -33,33 +33,41 @@ void incluir_usuario(usuario *Usuarios,int *qtdCadastro) //Falta fazer a verific
 	
 	aut = buscar(Usuarios,*qtdCadastro,cpf);
 	
+	if(aut == -1)
+	{
+		strcpy(user.cpf,cpf);
+		fflush(stdin);
+		printf("Digite o Nome :");
+		gets(user.nome);
+		fflush(stdin);
+		printf("Digite o Número :");
+		gets(user.nro);
+		fflush(stdin);
+		printf("Digite o CEP :");
+		gets(user.cep);
+		fflush(stdin);
+		printf("Digite o(s) email(s) :"); //Falta incluir repetição para colocar mais de 1 se quiser!!!
+		gets(user.emails[0]);
+		fflush(stdin);
+		printf("Digite o(s) telefone(s) :");
+		gets(user.telefone[0]);
+		fflush(stdin);
+		printf("Digite o data de nascimento dd/mm/aa :");
+		gets(user.datanasc);
+		fflush(stdin);
+		printf("Digite a profissão :");
+		gets(user.profissao);
 	
-	fflush(stdin);
-	printf("Digite o Nome :");
-	gets(user.nome);
-	fflush(stdin);
-	printf("Digite o Número :");
-	gets(user.nro);
-	fflush(stdin);
-	printf("Digite o CEP :");
-	gets(user.cep);
-	fflush(stdin);
-	printf("Digite o(s) email(s) :"); //Falta incluir repetição para colocar mais de 1 se quiser!!!
-	gets(user.emails[0]);
-	fflush(stdin);
-	printf("Digite o(s) telefone(s) :");
-	gets(user.telefone[0]);
-	fflush(stdin);
-	printf("Digite o data de nascimento dd/mm/aa :");
-	gets(user.datanasc);
-	fflush(stdin);
-	printf("Digite a profissão :");
-	gets(user.profissao);
+		fflush(stdin);
 	
-	fflush(stdin);
+		Usuarios[*qtdCadastro] = user;
+		(*qtdCadastro)++;
+		printf("\nUsuário cadastrado com sucesso !\n\n");
+	}
+	else
+		printf("Usuário já está cadastrado !\n\n");
 	
-	Usuarios[*qtdCadastro] = user;
-	(*qtdCadastro)++;
+	system("pause");
 	
 }
 
@@ -173,7 +181,8 @@ int excluir_usuario(usuario *Usuarios,int *pos)
 			{
 				Usuarios[j] = Usuarios[j+1];
 			}
-			(*pos)++;
+			(*pos)--;
+			
 			return 1;
 		}
 		
@@ -181,11 +190,96 @@ int excluir_usuario(usuario *Usuarios,int *pos)
 	
 }
 
-/*void alterar_usuario(usuario *Vetor,int pos)
+void alterar_usuario(usuario *Vetor,int pos)
 {
-	int i;
-	for(i=o;i<pos)
-}*/
+	char cpf[15];
+	int posicao,op;
+	system("cls");
+	printf("Digite o cpf :\n");
+	fflush(stdin);
+	gets(cpf);
+	
+	posicao = buscar(Vetor,pos,cpf);
+	
+	if (posicao != -1)
+	{
+		do
+		{
+			printf("O que deseja alterar ?\n\n");
+			printf("1. Cpf\n");
+			printf("2. Nome\n");
+			printf("3. Número\n");
+			printf("4. Cep\n");
+			printf("5. E-mail(s)\n");
+			printf("6. telefone(s)\n");
+			printf("7. Data de nascimento\n");
+			printf("8. Profissão\n\n");
+		
+			scanf("%d",&op);
+			fflush(stdin);
+			system("cls");
+			switch(op)
+			{
+				case 1:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].cpf); //Falta verificar se está cadastrado !!
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 2:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].nome);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 3:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].nro);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 4:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].cep);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 5:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].emails[0]);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 6:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].telefone[0]);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 7:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].datanasc);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				case 8:
+					printf("\n\nDigite o novo dado :\n\n");
+					gets(Vetor[posicao].profissao);
+					printf("Dado alterado com sucesso !\n\n");
+					system("pause");
+					break;
+				default:
+					printf("opção inválida !");
+					system("pause");
+			}
+		}while(op>8 || op <=0);
+	}
+	else
+	{
+		printf("Usuário não cadastrado !\n\n");
+		system("pause");
+	}
+}
 
 int menu()
 {
@@ -231,7 +325,7 @@ void submenu_usuarios(int *pos)
 			incluir_usuario(usuarios,pos);
 			break;
 		case 4:
-			//alterar_usuario(usuarios,*pos);
+			alterar_usuario(usuarios,*pos);
 			break;
 		case 5:
 			excluir_usuario(usuarios,pos);
